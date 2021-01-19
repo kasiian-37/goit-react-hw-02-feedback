8e7f6ed7842305d688fbcb32f182ef8b29f44933
kasiian irina <karamio09@gmail.com>
2021-01-19 16:48:02 +0200

    Create Section.js

---
+++ src/components/Section/Section.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import styles from './Section.module.css';
+
+function Section({ title, children }) {
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.sectionTitle}>{title}</h2>
+      {children}
+    </section>
+  );
+}
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+export default Section;
