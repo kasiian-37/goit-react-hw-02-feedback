37dec97fd405fee295b2b169573c188f393dc70d
kasiian irina <karamio09@gmail.com>
2021-01-19 16:07:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import  { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Notification from './components/Notification/Notification';
+
+export default class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  feedbackHandler = e => {
+    this.setState(prevState => {
+      const buttonId = e.target.id;
+      return {
+        [buttonId]: prevState[buttonId] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((acc, value) => acc + value, 0);
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((100 / this.countTotalFeedback()) * this.state.good);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const feedbacksTotalAmount = this.countTotalFeedback();
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.feedbackHandler}
+          />
+        </Section>
+        <Section title="Statistics">
+          {feedbacksTotalAmount === 0 && (
+            <Notification message="No feedback given" />
+          )}
+
+          {feedbacksTotalAmount > 0 && (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={feedbacksTotalAmount}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
 }
-
-export default App;
